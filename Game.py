ca5e3b8eabc0affd62c6d4424a8ab1c81fa2a33f
PiotrFurtak <justpiotras@gmail.com>
2025-01-25 15:15:49 +0100

    Przerobienie Game.py na klasę Game i utworzenie Main.py, gdzie będzie interfejs sterujący wszystkim

--- Game.py
+++ Game.py
@@ -2,72 +2,73 @@ import pygame
 import sys
 from Car import *
 pygame.init()
-BLACK = (0,0,0)
-GREEN = (10,100,30)
-WHITE = (255,255,255)
-WINDOW_WIDTH, WINDOW_HEIGHT = (640,480)
 
-# Chcemy mieć pewność, że przekątna okna jest mniejsza lub równa niż bok bufora, żeby nie było dziur w rogach
-DIAGONAL = (WINDOW_HEIGHT**2 + WINDOW_WIDTH**2)**0.5
-DIAGONAL = int(DIAGONAL) + 1
-buffer = pygame.surface.Surface((DIAGONAL,DIAGONAL))
-window = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
-clock = pygame.time.Clock()
-player = Car(buffer,(100,150),pygame.image.load("imgs/red-car.png"),(48,18),is_player=True)
-pressed_keys = set()
+class Game:
+    def __init__(self,window):
+        self.running = False
+        self.WHITE = (255,255,255)
+        self.window = window
+        self.WINDOW_WIDTH, self.WINDOW_HEIGHT = window.get_size()
+        # Chcemy mieć pewność, że przekątna okna jest mniejsza lub równa niż bok bufora, żeby nie było dziur w rogach
+        self.DIAGONAL = (self.WINDOW_HEIGHT**2 + self.WINDOW_WIDTH**2)**0.5
+        self.DIAGONAL = int(self.DIAGONAL) + 1
 
-track = Sprite(buffer,(550,200),pygame.image.load("imgs/track.png"),(450,450))
-track.scale(2)
+        self.buffer = pygame.surface.Surface((self.DIAGONAL,self.DIAGONAL))
+        self.clock = pygame.time.Clock()
+        self.player = Car(self.buffer,(100,150),pygame.image.load("imgs/red-car.png"),(48,18),is_player=True)
+        self.pressed_keys = set()
 
-border = Sprite(buffer,(550,200),pygame.image.load("imgs/track-border.png"),(450,450))
-border.scale(2)
+        self.track = Sprite(self.buffer,(550,200),pygame.image.load("imgs/track.png"),(450,450))
+        self.track.scale(2)
 
-grass = []
-GRASS_IMG = pygame.image.load("imgs/grass.jpg")
-for x in range(-2000,2001,GRASS_IMG.get_width()):
-    for y in range(-2000,2001,GRASS_IMG.get_height()):
-        grass.append(Sprite(buffer,(x,y),GRASS_IMG,(0,0)))
+        self.border = Sprite(self.buffer,(550,200),pygame.image.load("imgs/track-border.png"),(450,450))
+        self.border.scale(2)
 
-def handle_events():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.display.quit()
-            sys.exit()
-        elif event.type == pygame.KEYDOWN:
-            pressed_keys.add(event.key)
-        elif event.type == pygame.KEYUP:
-            pressed_keys.discard(event.key)
+        self.grass = []
+        self.GRASS_IMG = pygame.image.load("imgs/grass.jpg")
+        for x in range(-2000,2001,self.GRASS_IMG.get_width()):
+            for y in range(-2000,2001,self.GRASS_IMG.get_height()):
+                self.grass.append(Sprite(self.buffer,(x,y),self.GRASS_IMG,(0,0)))
 
-    player.get_pressed_keys(pressed_keys)
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            elif event.type == pygame.KEYDOWN:
+                self.pressed_keys.add(event.key)
+            elif event.type == pygame.KEYUP:
+                self.pressed_keys.discard(event.key)
 
-def draw():
+        self.player.get_pressed_keys(self.pressed_keys)
 
-    # Umieść wszystkie sprite'y na buforze
-    for oSprite in grass:
-        oSprite.draw(DIAGONAL//2-player.x,DIAGONAL//2-player.y)
-    track.draw(DIAGONAL//2-player.x,DIAGONAL//2-player.y)
-    player.draw(DIAGONAL//2-player.x,DIAGONAL//2-player.y)
-    # player.oFrontWheels.draw(DIAGONAL//2-player.x,DIAGONAL//2-player.y)
+    def draw(self):
 
-    # Obróć bufor
-    rotated = pygame.transform.rotate(buffer,(-player.angle+90))
-    center = rotated.get_rect().center
-    # Wyświetl bufor
-    window.blit(rotated,(WINDOW_WIDTH//2-center[0],WINDOW_HEIGHT//2-center[1]))
-    
+        # Umieść wszystkie sprite'y na buforze
+        for oSprite in self.grass:
+            oSprite.draw(self.DIAGONAL//2-self.player.x,self.DIAGONAL//2-self.player.y)
+        self.track.draw(self.DIAGONAL//2-self.player.x,self.DIAGONAL//2-self.player.y)
+        self.player.draw(self.DIAGONAL//2-self.player.x,self.DIAGONAL//2-self.player.y)
+        # self.player.oFrontWheels.draw(self.DIAGONAL//2-self.player.x,self.DIAGONAL//2-self.player.y)
 
-
-def game_loop():
-    while True:
-        handle_events()
-        window.fill(WHITE)
-
-        player.move()
-        if player.isColliding(border):
-            player.velocity = -player.velocity
-            player.back()
+        # Obróć bufor
+        rotated = pygame.transform.rotate(self.buffer,(-self.player.angle+90))
+        center = rotated.get_rect().center
+        # Wyświetl bufor
+        self.window.blit(rotated,(self.WINDOW_WIDTH//2-center[0],self.WINDOW_HEIGHT//2-center[1]))
         
-        draw()
-        pygame.display.update()
-        clock.tick(60)
-game_loop()
+
+
+    def game_loop(self):
+        self.running = True
+        while self.running:
+            self.handle_events()
+            self.window.fill(self.WHITE)
+
+            self.player.move()
+            if self.player.isColliding(self.border):
+                self.player.velocity = -self.player.velocity
+                self.player.back()
+            
+            self.draw()
+            pygame.display.update()
+            self.clock.tick(60)
---
+++ Main.py
@@ -0,0 +1,9 @@
+import pygame
+from Game import Game
+WINDOW_WIDTH, WINDOW_HEIGHT = (640,480)
+window = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+oGame = Game(window)
+oGame.game_loop()
+window.fill((0,0,0))
+pygame.display.update()
+print("Hello world!")
